bf3a93c46092951c2439a92c8967ee62aba79a81
Jake Howard <jake@theorangeone.net>
2015-11-09 08:42:44 +0000

    Removed old URLs

--- project/urls.py
+++ project/urls.py
@@ -4,11 +4,6 @@ from project.pages.views import Custom404View
 
 urlpatterns = [
     url(r'^site-admin/', include(admin.site.urls)),
-    url(r'^student-robotics/', include('project.robotics.urls', namespace='robotics')),
-    url(r'^projects/', include('project.projects.urls', namespace='projects')),
-    url(r'^media/', include('project.media.urls', namespace='media')),
-    url(r'^setup/', include('project.setup.urls', namespace='setup')),
-    url(r'^college/', include('project.college.urls', namespace='college')),
     url(r'', include('project.pages.urls', namespace='pages'))
 ]
 
