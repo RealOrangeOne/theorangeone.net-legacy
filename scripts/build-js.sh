7d586c67f9127a8dccc7a08832df0d089f622fb7
Jake Howard <jake@theorangeone.net>
2015-11-09 14:13:17 +0000

    Imported dev page

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-cp node_modules/bootstrap/dist/js/bootstrap.js static/src/js/lib/
-cp node_modules/skrollr/src/skrollr.js static/src/js/lib/
-
 echo ">> Building Libraries..."
-uglifyjs node_modules/jquery/dist/jquery.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/jquery.js
-uglifyjs node_modules/markdown/lib/markdown.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/markdown.js
-uglifyjs static/src/js/lib/* --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/libs.js
+uglifyjs node_modules/jquery/dist/jquery.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/lib/jquery.js
+uglifyjs node_modules/markdown/lib/markdown.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/lib/markdown.js
+uglifyjs node_modules/bootstrap/dist/js/bootstrap.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/lib/bootstrap.js
+uglifyjs node_modules/skrollr/dist/skrollr.min.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/lib/skrollr.js
+uglifyjs static/build/js/lib/* --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/libs.js
+rm -rf static/build/js/lib/*
 
 echo ">> Building Application JS..."
 browserify -t reactify static/src/js/app.js -o static/build/js/app.js
-# uglifyjs static/build/js/app.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/app.js
-
-echo ">> Building Global Utilities..."
-uglifyjs static/src/js/utils.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/utils.js
+uglifyjs static/build/js/app.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/app.js
--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,12 +1,4 @@
 var React = require('react');
-var indexCarousel = require('./components/indexCarousel.js');
-
-require('./events.js');
-
-
-if ($('#index-carousel-container').length == 1) {
-    React.render(indexCarousel, $("#index-carousel-container")[0]);
-}
 
 $(window).load(function(){
     $(window).trigger('scroll').trigger('resize');
