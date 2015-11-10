ac360f454452047f8e97711ef8f3dd6fe1c6e1be
Jake Howard <jake@theorangeone.net>
2015-11-10 09:26:18 +0000

    Added template for dynamic projects component

--- static/src/js/components/project-images.js
+++ static/src/js/components/project-images.js
@@ -1,14 +1,37 @@
 var React = require('react');
 
+const projectData = [
+  {key: 1, title: "College", info: "", link: ""}
+  {key: 2, title: "Personal", info: "", link: ""}
+  {key: 3, title: "Work", info: "", link: ""}
+];
+
+
 var projectImages = React.createClass({
+  getInitialState: function () {
+    return {
+      hover: 0,
+    };
+  },
+
+  setHovering: function (key, evt) {
+    this.setState({hover: key});
+  },
   render: function () {
+    var isHovered;
+    var projectTiles = projectData.map(function (project) {
+      isHovered = (project.key == this.state.hover);
+      return (
+        <div className="col-sm-4">
+          
+        </div>
+      );
+    });
     return (
       <div className="container">
         <div className="panel panel-default">
           <div className="panel-body row">
-            <div className="col-xs-4">
-              <p>things</p>
-            </div>
+            { projectTiles }
           </div>
         </div>
       </div>
