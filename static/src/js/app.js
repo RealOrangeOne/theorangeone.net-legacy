60260ada948095f83bd1165f76df5b6549439bb0
Jake Howard <jake@theorangeone.net>
2015-11-10 08:59:52 +0000

    Moved project image section to component

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,6 +1,10 @@
 var React = require('react');
+var ProjectImages = require('./components/project-images');
+
 
 $(window).load(function(){
     $(window).trigger('scroll').trigger('resize');
     var s = skrollr.init();
 });
+
+React.render(<ProjectImages />, document.getElementById('project-images'));
---
+++ static/src/js/components/project-images.js
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var projectImages = React.createClass({
+  render: function () {
+    return (
+      <div className="container">
+        <div className="panel panel-default">
+          <div className="panel-body row">
+            <div className="col-xs-4">
+              <p>things</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = projectImages;
