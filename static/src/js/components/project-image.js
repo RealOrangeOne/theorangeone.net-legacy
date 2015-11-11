ca5b31d94671dc4abcf34011e8a3448e0529f51e
Jake Howard <jake@theorangeone.net>
2015-11-11 09:24:44 +0000

    Massive improvement to animation out

--- static/src/js/components/project-image.js
+++ static/src/js/components/project-image.js
@@ -5,23 +5,20 @@ var Col = ReactBootstrap.Col;
 
 var projectImage = React.createClass({
   render: function () {
-    var contents = this.props.isHovered ? 
-    (
-      <div className="animated zoomIn">
-        <h4>{this.props.data.title}</h4>
-        <p>{this.props.data.text}</p>
-        <a href={this.props.data.link} className="btn btn-default">
-          Find out more
-        </a> 
-      </div>    
-    ) : null;
+    var animationClass = this.props.isHovered ? 'zoomIn' : 'zoomOut';
     return (
       <Col sm={4}>
         <div className="wrapper" >
           <div className={"project " + this.props.data.className} 
             onMouseOver={this.props.onHover}
             onMouseLeave={this.props.onLeave} >
-            { contents }            
+            <div className={"animated " + animationClass}>
+              <h4>{this.props.data.title}</h4>
+              <p>{this.props.data.text}</p>
+              <a href={this.props.data.link} className="btn btn-default btn-small">
+                Find out more
+              </a> 
+            </div>     
           </div>
         </div>
       </Col>
