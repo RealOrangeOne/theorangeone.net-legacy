4d75169b027d37828679da42f5cd1a6328e84da0
Jake Howard <git@theorangeone.net>
2015-11-21 15:08:33 +0000

    Externalised jquery module

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -3,11 +3,13 @@
 set -e
 
 echo ">> Building Libraries..."
-uglifyjs node_modules/jquery/dist/jquery.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/lib/jquery.js
 uglifyjs node_modules/markdown/lib/markdown.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/lib/markdown.js
 uglifyjs node_modules/bootstrap/dist/js/bootstrap.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/lib/bootstrap.js
 uglifyjs static/build/js/lib/* --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/libs.js
-rm -rf static/build/js/lib/*
+rm -rf static/build/js/lib
+
+echo ">> Building JQuery..."
+uglifyjs node_modules/jquery/dist/jquery.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/jquery.js
 
 echo ">> Building Application JS..."
 browserify -t reactify static/src/js/app.js -o static/build/js/app.js
