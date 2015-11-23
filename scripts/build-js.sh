bde2140d29f35aa863a64ab8dddb5d436d00cc13
Jake Howard <git@theorangeone.net>
2015-11-23 14:21:04 +0000

    Hello browserify

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -13,5 +13,5 @@ echo ">> Building JQuery..."
 uglifyjs node_modules/jquery/dist/jquery.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/jquery.js
 
 echo ">> Building Application JS..."
-browserify -t reactify static/src/js/app.js -o static/build/js/app.js
+browserify -t [ babelify --presets [ es2015 react ] ] static/src/js/app.js -o static/build/js/app.js
 # uglifyjs static/build/js/app.js --compress --screw-ie8 --define --stats --keep-fnames -o static/build/js/app.js
