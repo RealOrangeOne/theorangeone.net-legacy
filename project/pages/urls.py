677e6a78d13d8141b0164d1fb675285a56bb467e
Jake Howard <git@theorangeone.net>
2015-11-25 19:39:11 +0000

    Added content template

--- project/pages/urls.py
+++ project/pages/urls.py
@@ -5,7 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^about/website/$', views.AboutWebsiteView.as_view(), name='about-website'),
     url(r'^about/$', views.AboutIndexView.as_view(), name='about'),
-    url(r'^no-js/$', views.NoJavascriptView.as_view(), name='no-js'),
-    url(r'^404/$', views.Custom404View.as_view(), name='404'),
     url(r'^$', views.IndexView.as_view(), name='index')
 ]
