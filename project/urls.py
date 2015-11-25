eb80c30b818dc429a489db1f9143a8af555a4445
Jake Howard <git@theorangeone.net>
2015-11-25 08:31:17 +0000

    Added testing urls

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import include, url
-from django.views.generic import TemplateView
 from django.contrib import admin
-from project.pages.views import Custom404View
+from project.pages.views import Custom404View, NoJavascriptView
 
 
 urlpatterns = [
     url(r'^site-admin/', include(admin.site.urls)),
-    url(r'^404/', TemplateView.as_view(template_name='core/404.html')),
+    url(r'^404/', Custom404View.as_view(), name="404"),
+    url(r'^no-js/', NoJavascriptView.as_view(), name="no-js"),
     url(r'', include('project.pages.urls', namespace='pages'))
 ]
-
 
 handler404 = Custom404View
