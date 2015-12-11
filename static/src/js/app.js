407325674cc4109d082ad1dc1cc1e213d76b5183
Jake Howard <git@theorangeone.net>
2015-12-11 08:44:48 +0000

    Changed folder structure for scripts

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -3,8 +3,8 @@
 require('./utils.js');
 
 var React = require('react');
-var ProjectImagesTypes = require('./components/project-images-types');
-var ProjectImagesMain = require('./components/project-images-main');
+var ProjectImagesTypes = require('./components/index/project-images-types');
+var ProjectImagesMain = require('./components/index/project-images-main');
 
 if ($('body').hasClass('index')) {  // Render components on index
   React.render(<ProjectImagesTypes />, document.getElementById('project-images-types'));
