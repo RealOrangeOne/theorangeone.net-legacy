1bbb625255cd1541c6ccf08fab260d185c838974
Jake Howard <git@theorangeone.net>
2016-01-03 17:51:14 +0000

    React-ify the navbar

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -6,8 +6,13 @@ import React from 'react';
 import ProjectImagesTypes from './components/index/project-images-types';
 import ProjectImagesMain from './components/index/project-images-main';
 
+import NavBar from './components/navbar/navbar';
 
 if ($('body').hasClass('index')) {  // Render components on index
   React.render(<ProjectImagesTypes />, document.getElementById('project-images-types'));
   React.render(<ProjectImagesMain />, document.getElementById('project-images-main'));
+}
+
+if ($('#navbar-test')) {
+  React.render(<NavBar />, document.getElementById('navbar-test'));
 }
---
+++ static/src/js/components/navbar/navbar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+var ReactBootstrap = require('react-bootstrap');
+var {
+  Navbar,
+  Nav,
+  MenuItem,
+  NavItem,
+  NavDropdown
+} = ReactBootstrap;
+
+export default class NavBar extends React.Component {
+  render() {
+    return (
+      <span>
+        <div id="navbar-anchor" />
+        <div id="navbar-container" className="align h4">
+          <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand visible-xs"><i className="icon ion-compass"></i> Navigation</a>
+              </div>
+              <div className="collapse navbar-collapse" id="navbar">
+                <ul className="nav navbar-nav home-button hidden-sm">
+                  <li className="active home"><a href=""><i className="icon ion-home"></i></a></li>
+                </ul>
+                <ul className="nav navbar-nav" id="navigation">
+                  <li className="dropdown">
+                    <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      Projects <i className="icon ion-ios-arrow-up navbar-icon h4"></i>
+                    </a>
+                    <ul className="dropdown-menu dropup">
+                      <li><a href="#"><i className="icon ion-social-freebsd-devil"></i> Pithos</a></li>
+                      <li><a href=""><i className="icon ion-ios-monitor"></i> BSOD Enabler</a></li>
+                      <li><a href=""><i className="icon ion-chatbox-working"></i> Hipchat Emoticons for All</a></li>
+                      <li><a href=""><i className="icon ion-social-windows"></i> Custom PC</a></li>
+                      <li><a href=""><i className="icon ion-android-more-vertical"></i> All Projects...</a></li>
+                    </ul>
+                  </li>
+                  <li className="dropdown">
+                    <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      Code <i className="icon ion-ios-arrow-up navbar-icon h4"></i>
+                    </a>
+                    <ul className="dropdown-menu dropup">
+                      <li><a href="#"><i className="icon ion-code-working"></i> Code Challenges</a></li>
+                      <li><a href=""><i className="icon ion-ios-circle-filled"></i> Morse Code Decoder</a></li>
+                      <li><a href=""><i className="icon ion-ios-game-controller-a"></i> The Wiki Game Solver</a></li>
+                      <li><a href=""><span className="glyphicon glyphicon-print" aria-hidden="true"></span>  Printr</a></li>
+                    </ul>
+                  </li>
+                  <NavItem href="#">Link 1</NavItem>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </span>
+    );
+  }
+}
