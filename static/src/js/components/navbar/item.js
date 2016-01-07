c6f883f88c58a88926bf164c400fca8be9639192
Jake Howard <git@theorangeone.net>
2016-01-07 18:53:24 +0000

    Conditional render of icon component

--- static/src/js/components/navbar/item.js
+++ static/src/js/components/navbar/item.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default class Item extends React.Component {
   render() {
+    let icon;
+    if (this.props.icon) {
+      icon = (
+        <i className={'icon ' + this.props.icon}></i>
+      );
+    }
     return (
-      <li><a href={this.props.href}><i className={'icon ' + this.props.icon}></i> {this.props.children}</a></li>
+      <li><a href={this.props.href}>{icon} {this.props.children}</a></li>
     );
   }
 }
