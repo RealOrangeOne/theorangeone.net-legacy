4d7dd9163da4c89e6fc25d7b8d203c1cff87e035
Jake Howard <git@theorangeone.net>
2016-01-11 17:16:44 +0000

    Added previous items to navbar

--- static/src/js/components/navbar/navbar.js
+++ static/src/js/components/navbar/navbar.js
@@ -29,7 +29,35 @@ export default class NavBar extends React.Component {
                     <Item href="#" icon="ion-ios-circle-filled">BSOD Enabler</Item>
                     <Item href="#" icon="ion-ios-game-controller-a">The Wiki Game Solver</Item>
                   </Dropdown>
-                  <Item href="#">Link 1</Item>
+                  <Dropdown title="College">
+                    <Item href="#">Student Robotics</Item>
+                    <Item href="#" icon="ion-cube">Attack on Blocks Game</Item>
+                    <Item href="#">Student Server</Item>
+                    <Item href="#" icon="ion-ios-paper">EPQ</Item>
+                    <Item href="#">Wall of Sheep</Item>
+                  </Dropdown>
+                  <Dropdown title="Setup">
+                    <Item href="#">&#128129; Desk</Item>
+                    <Item href="#" icon="ion-briefcase">Work</Item>
+                    <Item href="#" icon="ion-model-s">On The Go</Item>
+                    <Item href="#" icon="ion-android-phone-portrait">Devices</Item>
+                    <Item href="#">Servers</Item>
+                  </Dropdown>
+                  <Dropdown title="Work">
+                    <Item href="#" icon="ion-form-repo">Projects</Item>
+                    <Item href="#" icon="ion-android-desktop">Setup</Item>
+                  </Dropdown>
+                  <Dropdown title="Media">
+                    <Item href="#" icon="ion-social-youtube">YouTube Channel</Item>
+                    <Item href="#" icon="ion-social-rss">Feed</Item>
+                    <Item href="#" icon="ion-social-camera">Gallery</Item>
+                  </Dropdown>
+                  <Item href="#">Links</Item>
+                  <Dropdown title="About">
+                    <Item href="#" icon="ion-android-person">Me</Item>
+                    <Item href="#" icon="">Website</Item>
+                    <Item href="#" icon="ion-android-contacts">Contact Me</Item>
+                  </Dropdown>
                 </ul>
               </div>
             </div>
