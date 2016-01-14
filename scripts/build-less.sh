bd4f020328661b0e21755b15b3897e2009a92634
Jake Howard <git@theorangeone.net>
2016-01-14 19:12:23 +0000

    Added conditional LESS builds for production

---
+++ scripts/build-less.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $@ == **prod** ]]
+then
+  echo ">>> WARNING: Building in Production Mode!"
+fi
+
+echo ">> Building LESS..."
+lessc --silent static/src/less/style.less static/build/css/style.css
+
+if [[ $@ == **prod** ]]
+then
+  echo ">> Compressing LESS..."
+  cleancss -d --s0 -o static/build/css/style.css static/build/css/style.css
+fi
