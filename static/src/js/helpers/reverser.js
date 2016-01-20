bdfad83ad77846436bf56f937462430d48c91b8f
Jake Howard <git@theorangeone.net>
2016-01-20 18:52:04 +0000

    Allow args to be passed to reverser

--- static/src/js/helpers/reverser.js
+++ static/src/js/helpers/reverser.js
@@ -1,11 +1,19 @@
-export default function Reverser(ident) {
+export default function Reverser(ident, args) {
+  let body = {};
+  if (ident) {
+    body.ident = ident;
+  }
+  if (args) {
+    body.args = args;
+  }
+  body = JSON.stringify(body);
   return fetch('/reverse/', {
     method: 'post',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ident})
+    body
   }).then(function (response) {
     if (response.status === 302) {
       return response;
