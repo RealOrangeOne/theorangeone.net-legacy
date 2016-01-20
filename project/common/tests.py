060d929d94f7dcadfd5ef0142e0d51dc26cb9ef9
Jake Howard <git@theorangeone.net>
2016-01-20 19:28:02 +0000

    Added tests for project reverser

--- project/common/tests.py
+++ project/common/tests.py
@@ -14,3 +14,15 @@ class CustomTemplateTestCase(TestCase):
         response = self.client.get(reverse(self.template))
         for item in ['html_title', 'body_class', 'js_redirect']:
             self.assertIn(item, response.context)
+
+
+class ReverserTestCase(TestCase):
+    REVERSER_IDENT = 'reverser:reverser'
+    def test_reverser(self):
+        response = self.client.post(reverse(self.REVERSER_IDENT), data={'ident': 'pages:index'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.data, reverse('pages:index'))
+
+    def test_invalid_reverser(self):
+        response = self.client.post(reverse(self.REVERSER_IDENT), data={'ident': 'pages:i-dont-exist'})
+        self.assertEqual(response.status_code, 404)
