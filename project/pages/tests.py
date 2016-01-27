e95656debca60ebe3e3f287e52f7c5392e3bd2c0
Jake Howard <git@theorangeone.net>
2016-01-27 18:13:41 +0000

    refactor tests

--- project/pages/tests.py
+++ project/pages/tests.py
@@ -2,20 +2,16 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 
-class IndexTestCase(TestCase):
-    def test_accessable(self):
-        response = self.client.get(reverse('pages:index'))
-        self.assertEqual(response.status_code, 200)
-
-
-class AboutWebsiteTestCase(TestCase):
-    def test_accessable(self):
+class AboutTestCase(TestCase):
+    def test_website_accessable(self):
         response = self.client.get(reverse('about:website'))
         self.assertEqual(response.status_code, 200)
 
+    def test_me_accessable(self):
+        response = self.client.get(reverse('about:me'))
+        self.assertEqual(response.status_code, 200)
 
-class AboutIndexTestCase(TestCase):
-    def test_accessable(self):
+    def test_index_accessable(self):
         response = self.client.get(reverse('about:index'))
         self.assertEqual(response.status_code, 200)
 
@@ -33,21 +29,27 @@ class AboutIndexTestCase(TestCase):
         self.assertContains(response, 'Already Sent')
 
 
-class Custom404TestCase(TestCase):
-    def test_accessable(self):
+class CorePagesTestCase(TestCase):
+    def test_404_accessable(self):
         response = self.client.get(reverse('404'))
         self.assertEqual(response.status_code, 404)
 
-
-class NoJavascriptTestCase(TestCase):
-    def test_accessable(self):
+    def test_no_js_accessable(self):
         response = self.client.get(reverse('no-js'))
         self.assertEqual(response.status_code, 200)
 
+    def test_index_accessable(self):
+        response = self.client.get(reverse('pages:index'))
+        self.assertEqual(response.status_code, 200)
 
-class AllProjectsTestCase(TestCase):
-    def test_accessable(self):
+
+class ProjectsTestCase(TestCase):
+    def test_all_accessable(self):
         response = self.client.get(reverse('projects:all'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_test_project_accessable(self):
+        response = self.client.get(reverse('projects:project', args=['test']))
         self.assertEqual(response.status_code, 200)
 
 
