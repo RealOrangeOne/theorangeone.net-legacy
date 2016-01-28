1f29217b0a353cc80beb36f10c8126b1b84fd17b
Jake Howard <git@theorangeone.net>
2016-01-28 21:11:31 +0000

    Fix project urls

--- project/pages/urls/projects.py
+++ project/pages/urls/projects.py
@@ -3,6 +3,6 @@ from project.pages.views import projects
 
 
 urlpatterns = [
-    url(r'^projects/all/$', projects.AllView.as_view(), name="all"),
-    url(r'^projects/(?P<project>.+)/$', projects.ProjectView.as_view(), name="project")
+    url(r'^$', projects.AllView.as_view(), name="all"),
+    url(r'^(?P<project>.+)/$', projects.ProjectView.as_view(), name="project")
 ]
