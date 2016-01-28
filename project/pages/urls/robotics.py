a482b0824a9f996b11f1d814aa34f1f605637361
Jake Howard <git@theorangeone.net>
2016-01-28 21:09:19 +0000

    Refactor robotics pages

--- project/pages/urls/robotics.py
+++ project/pages/urls/robotics.py
@@ -3,8 +3,8 @@ from project.pages.views import robotics
 
 
 urlpatterns = [
-    url(r'^robotics/$', robotics.IndexView.as_view(), name="index"),
-    url(r'^robotics/2014/$', robotics.Index2014View.as_view(), name="2014-index"),
-    url(r'^robotics/2015/robot/$', robotics.Robot2015View.as_view(), name="2015-robot"),
-    url(r'^robotics/2015/$', robotics.Index2015View.as_view(), name="2015-index")
+    url(r'^$', robotics.IndexView.as_view(), name="index"),
+    url(r'^2014/$', robotics.Index2014View.as_view(), name="2014-index"),
+    url(r'^2015/robot/$', robotics.Robot2015View.as_view(), name="2015-robot"),
+    url(r'^2015/$', robotics.Index2015View.as_view(), name="2015-index")
 ]
