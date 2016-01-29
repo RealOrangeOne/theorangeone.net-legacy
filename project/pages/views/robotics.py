0c2d56094766e02806ef127b954f1ef744271e76
Jake Howard <git@theorangeone.net>
2016-01-29 23:07:12 +0000

    Added robotics pages titles

--- project/pages/views/robotics.py
+++ project/pages/views/robotics.py
@@ -3,15 +3,18 @@ from project.common.views import CustomTemplate, MarkdownView
 
 class IndexView(CustomTemplate):
     template_name = 'robotics/index.html'
+    html_title = 'Student Robotics'
 
 
 # 2015
 class Index2015View(CustomTemplate):
     template_name = 'robotics/2015-index.html'
+    html_title = 'Student Robotics 2015'
 
 
 class Robot2015View(CustomTemplate):
     template_name = 'robotics/2015-robot.html'
+    html_title = 'The Robot | SR2015'
 
 
 class Code2015View(MarkdownView):
@@ -21,3 +24,4 @@ class Code2015View(MarkdownView):
 # 2014
 class Index2014View(CustomTemplate):
     template_name = 'robotics/2014-index.html'
+    html_title = 'Student Robotics 2014'
