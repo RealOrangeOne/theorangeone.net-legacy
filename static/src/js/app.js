8b37ba7ad773d9c101db4e9097c6da8081a64ca4
RealOrangeOne <git@theorangeone.net>
2016-03-16 22:40:45 +0000

    Remove unused react components

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -7,14 +7,12 @@ import 'whatwg-fetch';
 
 import React from 'react';
 import ProjectImagesTypes from './components/index/project-images-types';
-import ProjectImagesMain from './components/index/project-images-main';
 
 import NavBar from './components/navbar/navbar';
 import Breadcrumbs from './components/breadcrumbs';
 
 if ($('body').hasClass('index')) {  // Render components on index
   React.render(<ProjectImagesTypes />, document.getElementById('project-images-types'));
-  React.render(<ProjectImagesMain />, document.getElementById('project-images-main'));
 }
 
 if ($('navbar').length > 0) {
