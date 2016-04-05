1db8e2910fa6ef8f2770a31db8ed134a3e63d4dc
Jake Howard <git@theorangeone.net>
2016-04-05 22:07:16 +0100

    Add about contact form page

--- project/pages/urls.py
+++ project/pages/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
-from .views import page_view, index_view
+from .views import page_view, index_view, AboutView
 
 
 urlpatterns = [
-    url(r'^(?P<path>.*)/', page_view, name='page'),
-    url(r'^$', index_view, name='page'),
+    url(r'^about/?$', AboutView.as_view(), name='about'),
+    url(r'^(?P<path>.*)', page_view, name='page'),
 ]
--- project/pages/views.py
+++ project/pages/views.py
@@ -29,3 +29,14 @@ def page_view(request, path):
 
 def index_view(request):
     return page_view(request, 'index')
+
+
+class AboutView(FormView):
+    template_name = 'about/index.html'
+    success_url = '/about/?sent'
+    form_class = ContactForm
+
+    def get_context_data(self, **kwargs):
+        context = dict(super().get_context_data(**kwargs), **get_context('/about'))
+        context['sent'] = 'sent' not in self.request.GET
+        return context
