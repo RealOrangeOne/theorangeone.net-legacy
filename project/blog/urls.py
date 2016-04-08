93c3805ba64141589a455b3060cf99c43fdebdcf
Jake Howard <git@theorangeone.net>
2016-04-08 23:12:33 +0100

    Remove need for trailing slash in blog urls

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<slug>.+)/$', views.BlogView.as_view(), name='blog-post'),
+    url(r'^(?P<slug>.+)/?$', views.BlogView.as_view(), name='blog-post'),
 ]
