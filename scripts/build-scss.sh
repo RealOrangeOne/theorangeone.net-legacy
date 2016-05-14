fb63d49640ab2fff2679ae62d82fd5e88595e385
Jake Howard <git@theorangeone.net>
2016-05-14 12:27:05 +0100

    Create scss build pipeline

---
+++ scripts/build-scss.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p theme/static/src/scss/css
+
+# scss can't import css, so copy them into src dir and change the extension!
+cp node_modules/animate.css/animate.css theme/static/src/scss/css/animate.scss
+
+node-sass theme/static/src/scss/index.scss theme/static/build/css/index.css
+
+# Cleanup
+rm -rf theme/static/src/scss/css
