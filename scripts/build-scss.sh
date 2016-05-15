6a94e9edf4ee631f1a26ebead9bd2fcab98da805
Jake Howard <git@theorangeone.net>
2016-05-15 17:44:52 +0100

    Add SCSS source maps

--- scripts/build-scss.sh
+++ scripts/build-scss.sh
@@ -7,7 +7,7 @@ mkdir -p theme/static/src/scss/css
 # scss can't import css, so copy them into src dir and change the extension!
 cp node_modules/animate.css/animate.css theme/static/src/scss/css/animate.scss
 
-node-sass theme/static/src/scss/index.scss theme/static/build/css/index.css
+node-sass theme/static/src/scss/index.scss theme/static/build/css/index.css --source-map-embed
 
 # Cleanup
 rm -rf theme/static/src/scss/css
