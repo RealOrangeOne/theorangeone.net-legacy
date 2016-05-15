d10b5217f3228f75a0c876b424b1eda9905c7d49
Jake Howard <git@theorangeone.net>
2016-05-15 20:57:03 +0100

    Create generation script

---
+++ scripts/generate-site.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "> Creating Site..."
+
+# Build the static data
+echo ">> Building static data..."
+npm run build $@
+
+echo ">> Compiling site pages..."
+pelican -v
+
+echo "> Build complete."
