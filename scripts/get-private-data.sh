df147ed3cb2534ebb5de87e66b5fd31122587cb5
Jake Howard <RealOrangeOne@users.noreply.github.com>
2016-05-23 09:04:34 +0100

    Move private data repo to github

--- scripts/get-private-data.sh
+++ scripts/get-private-data.sh
@@ -6,4 +6,4 @@ echo ">> Removing old Private Data..."
 rm -rf private/
 
 echo ">> Getting Private Data..."
-git clone git@bitbucket.org:TheOrangeOne/theorangeone.net-site-private-data.git --branch master --single-branch private/
+git clone git@github.com:RealOrangeOne/theorangeone.net-site-private-data.git --branch master --single-branch private/
