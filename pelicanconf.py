12970690a65f777410a16178a5e6079ad066cdbc
Jake Howard <git@theorangeone.net>
2016-05-28 22:59:58 +0100

    Cleanup settings file

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,22 +5,25 @@ from __future__ import unicode_literals
 import sys, os
 sys.path.insert(0, os.path.realpath('./plugins'))
 
+# Global core settings
 AUTHOR = 'Jake Howard'
 SITENAME = 'TheOrangeOne'
 SITEURL = 'http://theorangeone.net'
-
 PATH = 'content'
-
 TIMEZONE = 'Europe/London'
-
 DEFAULT_LANG = 'en'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+PAGE_PATHS = ["pages"]
+THEME = "theme"
+THEME_STATIC_DIR = "static"
+THEME_STATIC_PATHS = ['static/build']
+STATIC_PATHS = ["assets", "assets/favicon.ico"]
+EXTRA_PATH_METADATA = {
+    "assets/favicon.ico": {"path": "favicon.ico"}
+}
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_PAGINATION = False
+DELETE_OUTPUT_DIRECTORY = True
+SLUGIFY_SOURCE = 'basename'
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -32,50 +35,31 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 import social
 SOCIAL = social.generate()
 
-
-DEFAULT_PAGINATION = False
-DELETE_OUTPUT_DIRECTORY = True
-SLUGIFY_SOURCE = 'basename'
-
-PAGE_PATHS = ["pages"]
-PAGE_SAVE_AS = "{slug}/index.html"
-PAGE_URL = "{slug}"
-
-THEME = "theme"
-THEME_STATIC_DIR = "static"
-THEME_STATIC_PATHS = ['static/build']
-STATIC_PATHS = ["assets", "assets/favicon.ico"]
-EXTRA_PATH_METADATA = {
-    "assets/favicon.ico": {"path": "favicon.ico"}
-}
-
-ARTICLE_SAVE_AS = "{category}/{slug}/index.html"
-ARTICLE_URL = "{category}/{slug}/"
-
-TAG_URL = "blog/tag/{slug}/"
-TAG_SAVE_AS = "blog/tag/{slug}/index.html"
-
-TAGS_URL = "tags/"
-TAGS_SAVE_AS = "tags/index.html"
-
-AUTHOR_URL = "author/{slug}/"
-AUTHOR_SAVE_AS = "author/{slug}/index.html"
-
+# Disable some pages
+TAG_URL = False
+TAG_SAVE_AS = False
+TAGS_SAVE_AS = False
 AUTHORS_URL = False
 AUTHORS_SAVE_AS = False
-
+CATEGORIES_SAVE_AS = False
 ARCHIVES_URL = False
 ARCHIVES_SAVE_AS = False
 
+# Override page URLs
+PAGE_SAVE_AS = "{slug}/index.html"
+PAGE_URL = "{slug}"
+ARTICLE_SAVE_AS = "{category}/{slug}/index.html"
+ARTICLE_URL = "{category}/{slug}/"
+AUTHOR_URL = "author/{slug}/"
+AUTHOR_SAVE_AS = "author/{slug}/index.html"
 CATEGORY_SAVE_AS = "{slug}/index.html"
 CATEGORY_URL = "{slug}/"
 
-CATEGORIES_SAVE_AS = False
-USE_FOLDER_AS_CATEGORY = True
-
+# Add ATOM feed
 FEED_ATOM = 'feed.atom'
 FEED_DOMAIN = SITEURL
 
+# Setup plugins
 PLUGIN_PATHS = ["pelican_plugins"]
 PLUGINS = ["sitemap", "filetime_from_git", "pelican-jinja2content"]
 
@@ -83,13 +67,14 @@ SITEMAP = {
     "format": "xml"
 }
 
+# Setup markdown extensions
 from fontawesome_markdown import FontAwesomeExtension
 MD_EXTENSIONS = [FontAwesomeExtension(), 'codehilite(css_class=highlight)', 'extra']
 
+# Setup jinja2 filters
 import filters
 JINJA_FILTERS = {
     "datetime": filters.format_datetime,
     "raw": filters.html_to_raw,
     "category_find": filters.category_find
 }
-# Extra context
