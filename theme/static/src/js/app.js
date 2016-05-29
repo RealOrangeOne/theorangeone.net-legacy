73fcc3bfc855d032d41741ad3103a9d2ca52e60f
Jake Howard <git@theorangeone.net>
2016-05-29 23:25:23 +0100

    remove debug

--- theme/static/src/js/app.js
+++ theme/static/src/js/app.js
@@ -2,6 +2,5 @@ import './creative';
 
 $('.image').each(function () {
   const ele = $(this);
-  console.log('ITTERATING', ele.data('image'));
   ele.css('background-image', 'url(' + ele.data('image') + ')');
 });
