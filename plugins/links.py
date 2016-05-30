5927303b21b2be77fd174ef925df79121e40a78c
Jake Howard <git@theorangeone.net>
2016-05-30 17:39:43 +0100

    Add more links

--- plugins/links.py
+++ plugins/links.py
@@ -12,14 +12,16 @@ def social():
         "twitter": SocialLink("Twitter", "https://twitter.com/RealOrangeOne", "fa-twitter"),
         "reddit": SocialLink("Reddit", "https://reddit.com/user/RealOrangeOne", "fa-reddit"),
         "instagram": SocialLink("Instagram", "https://instagram.com/RealOrangeOne", "fa-instagram"),
-        "youtube": SocialLink("YouTube", "https://youtube.com/user/TheOrangeOneOfficial", "fa-youtube")
+        "youtube": SocialLink("YouTube", "https://youtube.com/user/TheOrangeOneOfficial", "fa-youtube"),
+        "flickr": SocialLink("Flickr", "https://www.flickr.com/photos/theorangeone/", "fa-flickr")
     }
 
 
 def index_projects():
     projects = [
         ProjectLink("Student Robotics", "/robotics/", "https://c2.staticflickr.com/8/7711/17122633430_e1b599fe47.jpg"),
-        ProjectLink("Dotfiles", "/projects/dotfiles/", "http://jleajones.com/assets/images/dotfiles.png")
+        ProjectLink("Dotfiles", "/projects/dotfiles/", "http://jleajones.com/assets/images/dotfiles.png"),
+        ProjectLink("Custom PC", "/setup/custom-pc/", "https://c2.staticflickr.com/8/7083/27071954860_f6096ccce6.jpg")
     ]
     shuffle(projects)
     return projects
