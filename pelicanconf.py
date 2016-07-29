76cf8b5fe1f4d95f810bbbda316b56b7d77eeac8
RealOrangeOne <git@theorangeone.net>
2016-07-29 12:00:06 +0100

    minify HTML

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
@@ -37,14 +36,14 @@ AUTHORS_SAVE_AS = False
 CATEGORIES_SAVE_AS = False
 ARCHIVES_URL = False
 ARCHIVES_SAVE_AS = False
+AUTHOR_URL = False
+AUTHOR_SAVE_AS = False
 
 # Override page URLs
 PAGE_SAVE_AS = "{slug}/index.html"
 PAGE_URL = "{slug}"
 ARTICLE_SAVE_AS = "{category}/{slug}/index.html"
 ARTICLE_URL = "{category}/{slug}/"
-AUTHOR_URL = "author/{slug}/"
-AUTHOR_SAVE_AS = "author/{slug}/index.html"
 CATEGORY_SAVE_AS = "{slug}/index.html"
 CATEGORY_URL = "{slug}/"
 
@@ -54,13 +53,22 @@ FEED_DOMAIN = SITEURL
 
 # Setup plugins
 PLUGIN_PATHS = ["pelican_plugins", "plugins"]
-PLUGINS = ["sitemap", "filetime_from_git", "pelican-jinja2content", "metatags", "autopages"]
+PLUGINS = [
+    "sitemap",
+    "filetime_from_git",
+    "pelican-jinja2content",
+    "metatags",
+    "autopages",
+    "minify"
+]
 
 SITEMAP = {
     "format": "xml"
 }
-
 CATEGORY_PAGE_PATH = "theme/templates/categories"
+MINIFY = {
+    "remove_comments": True
+}
 
 # Setup markdown extensions
 from fontawesome_markdown import FontAwesomeExtension
