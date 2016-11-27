04d307d6964242a5b09d40b470858ee5aaa634de
Jake Howard <git@theorangeone.net>
2016-11-27 20:55:28 +0000

    Cleanup creative JS

--- static/src/js/creative/creative.js
+++ static/src/js/creative/creative.js
@@ -2,17 +2,18 @@
  * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ * (Edited for theorangeone.net)
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+const NAVBAR_HEIGHT = $('#main-nav').height();
+
 $('a.page-scroll').bind('click', function(event) {
   const anchor = $(this);
   $('html, body').stop().animate(
     {
-      scrollTop: ($(anchor.attr('href')).offset().top - 50)
+      scrollTop: ($(anchor.attr('href')).offset().top - NAVBAR_HEIGHT)
     },
-    1250,
-    'easeInOutExpo'
+    750
   );
   event.preventDefault();
 });
@@ -20,18 +21,10 @@ $('a.page-scroll').bind('click', function(event) {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
   target: '.navbar-fixed-top',
-  offset: 51
+  offset: NAVBAR_HEIGHT + 1
 });
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
   $('.navbar-toggle:visible').click();
-});
-
-
-// Offset for Main Navigation
-$('#main-nav').affix({
-  offset: {
-    top: 50
-  }
 });
--- static/src/js/creative/index.js
+++ static/src/js/creative/index.js
@@ -1,6 +1,4 @@
-import 'jquery.easing';
 import './creative';
-
 import WOW from 'wow.js';
 
 new WOW().init();
