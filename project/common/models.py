52870818eadf10dfbfe2f5058264b5132afc7c1a
Jake Howard <git@theorangeone.net>
2016-11-28 21:56:44 +0000

    add created and modified fields

--- project/common/models.py
+++ project/common/models.py
@@ -1,8 +1,12 @@
 from wagtail.wagtailcore.models import Page
+from django.db import models
 
 
 class Entity(Page):
     is_home = False
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
