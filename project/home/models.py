9769cc58c4c3221124a07695ad41b7c79fd32c73
Jake Howard <git@theorangeone.net>
2016-12-29 15:47:09 +0000

    Add blog pages to index

--- project/home/models.py
+++ project/home/models.py
@@ -1,7 +1,7 @@
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
-
+from project.blog.models import BlogPage
 from project.common.models import Entity
 
 
@@ -13,3 +13,8 @@ class HomePage(Entity):
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full")
     ]
+
+    def get_context(self, *args, **kwargs):
+        context = super().get_context(*args, **kwargs)
+        context['blog_posts'] = BlogPage.objects.live().order_by('-post_date')[:4]
+        return context
